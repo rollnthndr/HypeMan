d93937025d6ec8b1f73039e52965312330d5ac89
rollnthndr <rollnthndr@gmail.com>
2020-01-17 18:48:24 -0700

    testing udp

--- hypeman_listener.py
+++ hypeman_listener.py
@@ -4,80 +4,101 @@ import pathlib
 from config.config import APP_CONFIG, DISCORD_CONFIG
 import discord
 import asyncio
+import asyncio_dgram
 
 # set up the global logger at logging level set in app_settings.ini
 logging.config.fileConfig(
     pathlib.Path(APP_CONFIG["app"]["logging_config"]), disable_existing_loggers=True,
 )
-logger = logging.getLogger(APP_CONFIG["app"]["logging_level"])
+if APP_CONFIG["app"]["debugging"].lower() == "true":
+    logger = logging.getLogger("debug")
+else:
+    logger = logging.getLogger("info")
 
-class BaseBot(discord.Client):
-    pass
 
-
-class AirbossHypemanBot(BaseBot):
+class AirbossHypemanBot(discord.Client):
     def __init__(self):
         super().__init__()
+        logger.debug(f"Initializing AirbossHypemanBot")
         self.channel = None
+        self.client_id = DISCORD_CONFIG["airboss_hypeman"]["client_id"]
+        self.announce = APP_CONFIG["app"]["announce_bot_start"]
+        self.announce_msg = APP_CONFIG["app"]["announce_bot_msg"]
 
+    # Called when the bot has connected to discord and is ready.
     async def on_ready(self):
-        logger.info(f"{self.user.name} is logged onto Discord and mission ready.")
-        self.channel = self.get_channel(int(DISCORD_CONFIG["airboss_hypeman"]["channel_id"]))
+        logger.info(f"Bot logged onto Discord and mission ready.")
+
+        # now that bot is connected to discord, get the channel it's connected to
+        self.channel = self.get_channel(
+            int(DISCORD_CONFIG["airboss_hypeman"]["channel_id"])
+        )
+
+        # bot will announce in discord once it's connected if set in ini file
+        if str(self.announce).lower() == "true":
+            await self.channel.send(f"`{self.announce_msg}`")
 
     async def on_message(self, message):
-        logger.debug(f'{message.content}')
+        # if message on channel is from this bot, we don't want to do
+        # anything so just return.
+        if message.author.id == self.user.id:
+            return
+
+        logger.debug(f"{message.content}")
+
+        # check message if it contains a command
         if message.content.startswith("!boatstuff"):
             logger.debug("Airboss Hypeman received #boatstuff message.")
             if logger.level == logging.DEBUG:
-                await self.channel.send(f'```Hello from Airboss HypeMan```')
+                await self.channel.send(f"```Hello from Airboss HypeMan```")
 
     async def on_error(self, event, *args, **kwargs):
-        logger.debug(f'Error - {event}')
+        logger.debug(f"Bot error - {event}\n{args}")
 
 
 class HypeManListener:
-    def __init__(self):
+    def __init__(self, bot):
         logger.debug("Initializing HypeManListener")
-        
+
+        self.bot = bot
+
         self._host = APP_CONFIG["app"]["host"]
-        self._port = APP_CONFIG["app"]["port"]
-        
-        self._announce = bool(APP_CONFIG["app"]["announce_bot_start"])
-        
-        self._airboss_client_id = DISCORD_CONFIG["airboss_hypeman"]["client_id"]
-        
-        self._airboss = AirbossHypemanBot()
+        self._port = int(APP_CONFIG["app"]["port"])
 
-        self._start_listener()
-
-    def _start_listener(self):
+    async def start_listener(self):
         """Start Discord Bots"""
 
-        logger.debug("Starting listener")
-        # get the event loop and start each bot
-        loop = asyncio.get_event_loop()
+        # bind udp server to the host/port
+        stream = await asyncio_dgram.bind((self._host, self._port))
+        logger.info(f"Listening on {stream.sockname}")
 
-        # create bot tasks
-        loop.create_task(self._airboss.start(self._airboss_client_id))
-
-        try:
-            # run all tasks
-            logger.debug('Starting loop')
-            loop.run_forever()
-        except KeyboardInterrupt:
-            logger.info('Ctl-C received, shutting down...')
-            pass
-        finally:
-            loop.stop()
-
-        # if bots are to announce on Discord, send message.
-        # if self._announce:
-        # self._bot_hypeman.wait_until_ready()
-        # logger.debug("here")
+        # main listener loop
+        while 1:
+            # wait to receive data
+            data, remote_addr = await stream.recv()
+            logger.debug(f"UDP received - {data.decode()!r}")
 
 
 if __name__ == "__main__":
-    # we begin....
-    # logging.info('HypeMan listening...')
 
-    HypeManListener()
+    hp_bot = AirbossHypemanBot()
+    hm_listener = HypeManListener(hp_bot)
+
+    # get the event main loop to run the tasks asynchronously
+    loop = asyncio.get_event_loop()
+
+    # udp server task
+    loop.create_task(hm_listener.start_listener())
+
+    # discord bot task
+    loop.create_task(hm_listener.bot.start(hm_listener.bot.client_id))
+
+    try:
+        # run all tasks in infinite loop
+        logger.debug("Starting listener and bot.")
+        loop.run_forever()
+    except KeyboardInterrupt:
+        logger.info("Ctl-C received, shutting down.")
+    finally:
+        loop.stop()
+
