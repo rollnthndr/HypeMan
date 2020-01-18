af4ba379d0f434a9122d37718e7c12e13178efd6
RollnThndr <rollnthndr@gmail.com>
2020-01-18 00:30:32 -0700

    moved to legacy folder

--- server_info.bat
+++
@@ -1,8 +0,0 @@
-@echo on
-echo %1
-REM call C:\Users\DCSAdmin\Anaconda3\Scripts\activate.bat C:\Users\DCSAdmin\Anaconda3
-REM C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
-REM call C:\Users\reso\Anaconda3\Scripts\activate.bat C:\Users\reso\Anaconda3
-REM call C:\Users\jow\Anaconda3\Scripts\activate.bat C:\Users\jow\Anaconda3
-call C:\Hypeman\env\Scripts\activate.bat
-python server_info.py %1 > data\server_info.txt
