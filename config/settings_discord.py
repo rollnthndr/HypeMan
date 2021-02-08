258343ae8c788e5638851149f7e26a793856b042
RollnThndr <rollnthndr@gmail.com>
2021-02-08 16:11:56 -0700

    generic discord settings file

---
+++ config/settings_discord.py
@@ -0,0 +1,14 @@
+class HYPEMAN:
+    ID_CLIENT = 'Some ID'
+    # ID_CHANNEL = [
+    #     ['airboss'], [666334399998066718]
+    # ]
+    ID_CHANNEL = 1234567890
+    BOT_ANNOUNCE = True
+    BOT_ANNOUNCE_MSG = 'HypeMan standing by to standby.'
+
+class COMMAND:
+    ID_PRIVATE_COMMAND = 1234567890
+
+class MISC:
+    SERVER_NAME = Your Server Name'
