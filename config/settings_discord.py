678d7df792f6c6c688ea47bafab1b5d15c20fe81
RollnThndr <rollnthndr@gmail.com>
2021-02-09 13:22:51 -0700

    Blank discord settings file.

--- config/settings_discord.py
+++ config/settings_discord.py
@@ -1,19 +1,11 @@
 class HYPEMAN:
-<<<<<<< HEAD
-    ID_CLIENT = 'NjY2MzM0MTEyODgxMzExNzU1.XhyqAA.VV31Xazy62J2n0pk4Xg4msqtxXQ'
-    # ID_CHANNEL = [
-    #     ['airboss'], [666334399998066718]
-    # ]
-    ID_CHANNEL = 666334399998066718
-=======
     ID_CLIENT = 'Some ID'
     ID_CHANNEL = 1234567890
->>>>>>> 29ff653d2cabd5eda043e64ca3d1ff2feff27d16
     BOT_ANNOUNCE = True
     BOT_ANNOUNCE_MSG = 'HypeMan standing by to standby.'
 
 class COMMAND:
-    ID_PRIVATE_COMMAND = 361935537571102720
+    ID_PRIVATE_COMMAND = 1234567890
 
 class MISC:
-    SERVER_NAME = '416 Lynx Server'
+    SERVER_NAME = 'Your Server Name'
