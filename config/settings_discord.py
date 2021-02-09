f087a42b351c65ee3a27cb5e02b40594f24a54b6
RollnThndr <rollnthndr@gmail.com>
2021-02-09 14:00:32 -0700

    Stopped tracking discord settings file.

--- config/settings_discord.py
+++ config/settings_discord.py
@@ -1,11 +1,11 @@
 class HYPEMAN:
-    ID_CLIENT = 'Some ID'
-    ID_CHANNEL = 1234567890
+    ID_CLIENT = 'NjY2MzM0MTEyODgxMzExNzU1.XhyqAA.pbEjT1-gx7_14SGRmqvX9wj22xw'
+    ID_CHANNEL = 666334399998066718
     BOT_ANNOUNCE = True
     BOT_ANNOUNCE_MSG = 'HypeMan standing by to standby.'
 
 class COMMAND:
-    ID_PRIVATE_COMMAND = 1234567890
+    ID_PRIVATE_COMMAND = 361935537571102720
 
 class MISC:
-    SERVER_NAME = 'Your Server Name'
+    SERVER_NAME = 'Great Ballz Of Fire'
